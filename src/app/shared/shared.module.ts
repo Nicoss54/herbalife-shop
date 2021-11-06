044b559a61573b6de1fdabefa880c5043762f681
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2021-11-06 13:28:02 +0100

    feat(core): :heavy_plus_sign: add angular material dependencies for design system

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MaterialModule } from './material-design-system/material.module';
+
+@NgModule({
+  imports: [],
+  exports: [MaterialModule],
+  declarations: [],
+  providers: [],
+})
+export class SharedModule {}
