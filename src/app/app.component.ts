3ffed0a8705af1e23946cb833f7c7858ca1725f3
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2021-11-06 12:32:20 +0100

    chore: add linter and eslint configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'herbalife-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'herbalife-shop';
